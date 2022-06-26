6a3e1a95edaa126866493b3c0659b2499e0cf400
Balakrishna <balakrishnace@gmail.com>
2022-06-26 18:01:18 +0530

    Adding file

---
+++ learning.js
@@ -0,0 +1,14 @@
+let arr=[1,2,3,4,5,6]
+var num = 5
+
+for(var i=0; i<arr.length; i++)
+
+{
+    if (num = arr){
+        console.log(its is divisible)
+
+    }
+    else{
+        console.log(it is not divisible)
+    }
+}
